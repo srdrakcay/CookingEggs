3d27734b9e5e9a712d853943034c6826bd7588bc
Serdar Akçay <srdrakcay25@gmail.com>
2022-09-26 14:24:14 +0300

    first commit with alone

---
+++ app/src/main/java/com/serdar/cookingeggs/ui/SplashFragment.kt
@@ -0,0 +1,37 @@
+package com.serdar.cookingeggs.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.serdar.cookingeggs.R
+import com.serdar.cookingeggs.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        Handler().postDelayed(
+            {
+                    findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            },
+            900
+        )
+
+    }
+
+}
