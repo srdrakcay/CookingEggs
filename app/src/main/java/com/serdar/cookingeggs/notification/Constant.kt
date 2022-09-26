03431743b183579fdd10586c6d8ddd992b30cda1
Serdar Akçay <srdrakcay25@gmail.com>
2022-09-26 14:49:33 +0300

    DONE

---
+++ app/src/main/java/com/serdar/cookingeggs/notification/Constant.kt
@@ -0,0 +1,12 @@
+package com.serdar.cookingeggs.notification
+
+class Constant {
+    companion object {
+
+        const val CHANNEL_ID = "MyNotification"
+        const val CHANNEL_NAME = "MyNotificationChannel"
+
+        const val TITLE = "Cooking Egg"
+        const val DESCRIPTION = " Your Egg is Ready Now Bon Appetit "
+    }
+}
--- app/src/main/java/com/serdar/cookingeggs/ui/HomeFragment.kt
+++ app/src/main/java/com/serdar/cookingeggs/ui/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.serdar.cookingeggs.ui
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.serdar.cookingeggs.R
 import com.serdar.cookingeggs.databinding.FragmentHomeBinding
+import com.serdar.cookingeggs.notification.Constant.Companion.DESCRIPTION
+import com.serdar.cookingeggs.notification.Constant.Companion.TITLE
+import com.serdar.cookingeggs.notification.Notification
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -20,9 +23,40 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.egg1.setOnClickListener {
+            timer()
+        }
+        binding.egg2.setOnClickListener {
+            timer()
+        }
+        binding.egg3.setOnClickListener {
+            timer()
+        }
+        binding.egg4.setOnClickListener {
+            timer()
+        }
+        binding.egg5.setOnClickListener {
 
+            timer()
+        }
+    }
 
+    private fun timer(){
+        // add different time for the eggs
+        object : CountDownTimer(15000,1000)
+        {
 
+            override fun onTick(p0: Long)
 
+            {
+                binding.timer.text = "Kalan: ${p0 / 1000}"
+            }
+
+            override fun onFinish() {
+                binding.timer.text = "Your egg is ready now"
+                Notification.timerNotification(requireContext(), TITLE,DESCRIPTION)
+            }
+
+        }.start()
     }
 }
